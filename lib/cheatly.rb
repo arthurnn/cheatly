669c2933f79faa3762fac455975ab5a585842b65
Łukasz Niemier <lukasz@niemier.pl>
2013-12-02 10:55:14 +0100

    Extract adapters to different files

--- lib/cheatly.rb
+++ lib/cheatly.rb
@@ -11,4 +11,9 @@ require "cheatly/version"
 module Cheatly
   autoload :Sheet, "cheatly/sheet"
   autoload :CLI, "cheatly/cli"
+
+  module Adapter
+    autoload :File, "cheatly/adapter/file"
+    autoload :GitHub, "cheatly/adapter/github"
+  end
 end
---
+++ lib/cheatly/adapter/file.rb
@@ -0,0 +1,28 @@
+module Cheatly
+  module Adapter
+    class File
+      def find(name)
+        path = "sheets/#{name}.yml"
+        sheet_yaml = ::File.read(path)
+        yml = YAML.load(sheet_yaml).first
+        [yml.first, yml.last]
+      end
+
+      def all
+        Dir["sheets/*.yml"].map { |f| f.scan(/sheets\/(.*).yml/)[0][0] }
+      end
+
+      def create(name, body)
+        body = {name => body}.to_yaml
+        f = ::File.new("sheets/#{name}.yml", "w")
+        f.write(body)
+        f.close
+      end
+
+      def update(name, body)
+        File.delete("sheets/#{name}.yml")
+        create(name, body)
+      end
+    end
+  end
+end
