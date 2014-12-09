4e87d5ddc328c77b296f791fd89bd7dd5a580554
pdostal <git@pdostal.cz>
2014-12-09 13:11:00 +0100

    Add exceptions to CLI test

--- test/cli_test.rb
+++ test/cli_test.rb
@@ -3,20 +3,37 @@ require 'test_helper'
 class CliTest < MiniTest::Test
 
   def test_list
-    cli = Cheatly::CLI.new([], nopaginate: true, test: true)
-    cli.list
+    begin
+      cli = Cheatly::CLI.new([], nopaginate: true, test: true)
+      cli.list
+    rescue Exception => e
+      raise "#{e.message}"
+    end
 
-    cli = Cheatly::CLI.new([], local: true, nopaginate: true, test: true)
-    cli.list
+    begin
+      cli = Cheatly::CLI.new([], local: true, nopaginate: true, test: true)
+      cli.list
+    rescue Exception => e
+      raise "#{e.message}"
+    end
+
   end
 
   def test_show
-    cli = Cheatly::CLI.new([], nopaginate: true, test: true)
-    cli.show "markdown"
+    begin
+      cli = Cheatly::CLI.new([], nopaginate: true, test: true)
+      cli.show "markdown"
+    rescue Exception => e
+      raise "#{e.message}"
+    end
   end
 
   def test_help
-    cli = Cheatly::CLI.new([], nopaginate: true, test: true)
-    cli.help
+    begin
+      cli = Cheatly::CLI.new([], nopaginate: true, test: true)
+      cli.help
+    rescue Exception => e
+      raise "#{e.message}"
+    end
   end
 end
